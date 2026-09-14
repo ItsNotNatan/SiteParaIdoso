67249e3dbf53a5a323929cdf86021611bece7554
Natan Guimarães dos Santos <natanguimaraes158@gmail.com>
2026-09-14 11:55:59 -0300

    Add files via upload

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -9,27 +9,27 @@ import Login from '../pages/Login/Login';
 // Criação do mapa de navegação do site
 export const rotas = createBrowserRouter([
     {
-        // 1. Rota Principal (Mãe)
+        // 1. Rota Raiz: Ao entrar no site ('/'), redireciona imediatamente para o Login
         path: '/',
+        element: <Navigate to="/login" replace />
+    },
+    {
+        // 2. Rota de Login Isolada: Fora do Layout para não exibir a Navbar
+        path: '/login',
+        element: <Login />
+    },
+    {
+        // 3. Rotas Internas: Agrupadas pelo Layout (estas terão a Navbar visível)
         element: <Layout />,
         children: [
             {
-                index: true,
-                element: <Navigate to="/formulario" replace />
-            },
-            {
-                path: 'formulario',
+                path: '/formulario',
                 element: <Formulario />
             },
             {
-                path: 'dashboard',
+                path: '/dashboard',
                 element: <div><h1>Página de Dashboard em construção...</h1></div>
             }
         ]
-    },
-    {
-        // 2. Rota de Login Isolada (sem Navbar lateral/superior)
-        path: '/login',
-        element: <Login />
     }
 ]);
